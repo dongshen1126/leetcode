4bbaf53865e611eb924d76a4909326d700fecdcb
dongshen <dongshen@vipkid.com.cn>
2021-01-26 20:45:55 +0800

    update

--- leetcode.cpp
+++ leetcode.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 class Solution {
 public:
+    // 6
     string convert(string s, int numRows) {
         int slen = s.length();
         if (slen <= numRows || 1 == numRows) {
@@ -37,12 +38,115 @@ public:
         }
         return ret;
     }
+
+    // 7
+    int reverse(int x) {
+        int ret = 0;
+        while (x != 0) {
+            int tmp = x % 10;
+            x = x / 10;
+
+            if (ret > INT32_MAX / 10 || ret < INT32_MIN / 10) {
+                ret = 0;
+                break;
+            }
+
+            ret = ret * 10 + tmp;
+
+        }
+
+        return ret;
+    }
+
+    // 8
+    int myAtoi(string s) {
+        int ret = 0;
+        bool beginfound = false;
+        bool flag = true; // +
+        for (char c : s) {
+            if (false == beginfound) {
+                if ('+' == c) {
+                    beginfound = true;
+                    continue;
+                }
+                else if ('-' == c) {
+                    beginfound = true;
+                    flag = false;
+                    continue;
+                }
+                else if (c >= '0' && c <= '9') {
+                    beginfound = true;
+                }
+                else if (' ' == c) {
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (true == beginfound) {
+                if (c < '0' || c > '9') {
+                    break;
+                }
+
+                int tmp = c - '0';
+                if (false == flag) {
+                    tmp = 0 - tmp;
+                }
+
+                if (ret > INT32_MAX / 10 || ret == INT32_MAX / 10 && tmp > INT32_MAX % 10) {
+                    ret = INT32_MAX;
+                    break;
+                }
+                else if (ret < INT32_MIN / 10 || ret == INT32_MIN / 10 && tmp < INT32_MIN % 10) {
+                    ret = INT32_MIN;
+                    break;
+                }
+                ret = ret * 10 + tmp;
+            }
+        }
+
+        return ret;
+    }
+
+    // 9
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int tmp = 0;
+        int y = x;
+        while (x > 0) {
+            if (tmp > INT32_MAX / 10 || tmp == INT32_MAX / 10 && x%10 > INT32_MAX % 10) {
+                return false;
+            }
+            tmp = tmp * 10 + x % 10;
+            x = x / 10;
+        }
+
+        if (tmp == y) {
+            return true;
+        }
+
+        return false;
+    }
+    
 };
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //cout << -19 % 10 << endl;
+
+    Solution* s = new Solution();
+    /*int r = s->myAtoi("+-12");
+    cout << r << endl;*/
+
+    bool ret = s->isPalindrome(121);
+
+    cout << ret << endl;
 }
 
 
